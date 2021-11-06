fb1e2f290682b5dee2e870e548cba054628e25f4
Junito7 <ju.ni.to.7u@gmail.com>
2021-11-06 08:44:51 +0200

    ClickCounter added
    Added ClickCounter app

---
+++ ClickCounter/app/src/main/java/com/example/clickcounter/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.clickcounter
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button)
+        val textView = findViewById<TextView>(R.id.textView)
+        var count = 0
+        button.setOnClickListener{
+            count++
+            textView.text = count.toString()
+        }
+    }
+}
